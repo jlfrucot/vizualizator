728f19b8c4af493ecc55bf3285937fa00f3e4be1
jlfrucot <frucot.jeanlouis@free.fr>
2014-08-13 23:24:13 +0200

    Commentaire

--- vizualizator/valueddial.cpp
+++ vizualizator/valueddial.cpp
@@ -13,6 +13,8 @@ void ValuedDial::paintEvent(QPaintEvent *pe)
         int x = rect.width()/3;
         int y = rect.height()/2;
         painter.setPen(QPen(Qt::red));
-        int angle = (360-value())%360;
-        painter.drawText(QPoint(x,y),QString::number(angle)+"°");
+        /* Le calcul ci-desous permet d'avoir les valeurs dans le bon sens (antihoraire)
+         * et 0 à la place de 360
+         */
+        painter.drawText(QPoint(x,y),QString::number((360-value())%360)+"°");
 }
